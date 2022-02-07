76cf2aeff7bb7ebca79f823b60118174523f8962
aileen laksmono lie <aileen31.al@gmail.com>
2022-02-07 15:04:40 +0800

    mongodb connection

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,23 @@ require('dotenv').config();
 const cors = require('cors');
 
 const app = express();
+const mongoose = require('mongoose');
+
+
+mongoose.connect(process.env.MONGO_URL, () => {
+  console.log("connected to mongodb");
+});
+
+
 // default middleware used
 app.use(cors());
 app.use(express.json());
 
 require('./api')(app);
-const server_port = process.env.SERVER_PORT;
+
+//env file?
+const server_port = process.env.SERVER_PORT || 3000;
+
 app.listen(server_port, (error) => {
   console.log(`app is listening at port ${server_port}`);
 });
