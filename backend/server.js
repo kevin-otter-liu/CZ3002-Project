b881c32cf955530f8c196b3e766ab5120b0f355b
Kevin Liu <kevinliusingapore@gmail.com>
2022-02-08 15:27:15 +0800

    refactor: renamed index.js to server.js cause this is backend not frontend refactor: changed the entry file on nodemon and npm run to be server.js refactor: separated db instatiation file to db.js instead of being in server.js

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+//server.js
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const db = require('./db');
+
+// default middleware used
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// importing diff api routes
+const authApis = require('./routes/auth-routes');
+const transactionApis = require('./routes/transaction-routes');
+
+//registering routes
+app.use('/api/v1/auth', authApis);
+app.use('/api/v1/transaction', transactionApis);
+
+// initialise db, if db error dont start
+const SERVER_PORT = process.env.SERVER_PORT || 5000;
+db.initDb((err, db) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(SERVER_PORT, (error) => {
+      console.log(`app is listening at port ${SERVER_PORT}`);
+    });
+  }
+});
