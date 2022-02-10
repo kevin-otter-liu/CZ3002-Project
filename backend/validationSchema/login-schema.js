262e88bb8dcef38c77aa97cee7586b239831375a
Kevin Liu <kevinliusingapore@gmail.com>
2022-02-11 06:37:17 +0800

    Added request validation
    feature: validator folder that will contain all validators
    feature: added validation for auth controllers

---
+++ backend/validationSchema/login-schema.js
@@ -0,0 +1,46 @@
+// validator class that contains all validation middleware and schemas
+const Joi = require('joi');
+
+const loginSchema = Joi.object({
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ['com', 'net'] },
+    })
+    .min(1)
+    .max(30)
+    .required(),
+  password: Joi.string()
+    .min(16)
+    .max(60)
+    .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
+    .required(),
+});
+
+const validateParams = (
+  params,
+  options = {
+    errors: {
+      wrap: {
+        label: '',
+      },
+    },
+  }
+) => {
+  let param_errors = [];
+  validation = loginSchema.validate(params, options);
+  console.log(validation);
+  if (validation.error) {
+    validation.error.details.map((validationError) => {
+      let singleError = {
+        message: validationError.message,
+        path: validationError.path,
+      };
+      param_errors.push(singleError);
+    });
+  }
+
+  return [param_errors, validation.value];
+};
+
+module.exports = validateParams;
