3bbec1aaf44c02b21a7ee61f4ab84a1daebe32d6
Kevin Liu <kevinliusingapore@gmail.com>
2022-02-13 02:08:51 +0800

    Added integration tests for auth test: added tests for signin/singup/delete user apis feature: added delete user api

--- backend/routes/auth-routes.js
+++ backend/routes/auth-routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-
+const checkAuth = require('../middleware/checkAuth');
 // importing controllers
 const authControllers = require('../controllers/auth-controller');
 
 //definition of routes
 router.post('/sign-in', authControllers.signIn);
 router.post('/sign-up', authControllers.signUp);
+router.delete('/', [checkAuth], authControllers.deleteUser);
 
 module.exports = router;
