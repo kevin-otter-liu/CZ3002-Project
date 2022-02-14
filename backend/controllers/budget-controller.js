6a77216bccf840dda4dff3e5e8e3f8f0eb9e0329
aileen laksmono lie <aileen31.al@gmail.com>
2022-02-14 12:10:29 +0800

    budget controllers updated

--- backend/controllers/budget-controller.js
+++ backend/controllers/budget-controller.js
@@ -5,9 +5,9 @@ const createBudget = async (req, res, next) => {
   const start_date = new Date(req.body.period_start_date);
   const end_date = new Date(req.body.period_end_date);
   if (start_date > end_date) {
-    res.status(505).send("start date must be before end date");
-    }
-  else{
+    res.status(400).send("start date must be before end date");
+  } else {
+
     const budget_data = new Budget({
         user_id : req.body.user_id,//temporarily, would need to change to jwt
         amount : req.body.amount,
