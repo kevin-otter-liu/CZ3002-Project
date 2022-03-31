55967b9e1d6e535d5945aa56651199d1681f7822
Kevin Liu <kevinliusingapore@gmail.com>
2022-03-31 21:58:49 +0800

    Finished the formatting for the stats api

--- backend/controllers/stats-controller.js
+++ backend/controllers/stats-controller.js
@@ -1,4 +1,18 @@
 const Transaction = require('../models/Transaction');
+const monthNames = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'June',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+];
 
 const getStats = async (req, res, next) => {
   let { user } = req;
@@ -30,11 +44,19 @@ const getStats = async (req, res, next) => {
       },
     },
     {
-      $sort: {
+      $project: {
         year: 1,
         month: 1,
+        category: 1,
+        average: 1,
       },
     },
+    // {
+    //   $sort: {
+    //     year: 1,
+    //     month: 1,
+    //   },
+    // },
   ]);
   // find our stats for each category for user
   let user_result = await Transaction.aggregate([
@@ -48,11 +70,6 @@ const getStats = async (req, res, next) => {
       },
     },
     {
-      $sort: {
-        date_of_transaction: 1,
-      },
-    },
-    {
       $group: {
         _id: {
           year: { $year: '$date_of_transaction' },
@@ -62,11 +79,71 @@ const getStats = async (req, res, next) => {
         average: { $avg: '$amount' },
       },
     },
+    {
+      $sort: {
+        '_id.year': 1,
+        '_id.month': 1,
+        '_id.category': 1,
+      },
+    },
   ]);
+
+  // let formatted_response = [];
+  // console.log(all_result.length);
+  // console.log(user_result.length);
+
+  // for (let i = date.getFullYear() - 1; i < date.getFullYear(); i++) {
+  //   for (let j = 0; j < 12; j++) {
+  //     let formatted_object = {
+  //       month: `${monthNames[(date.getMonth() + j) % 12]} ${i}`,
+  //     };
+  //     formatted_response.push(formatted_object);
+  //   }
+  // }
+  // for (let i = 0; i < all_result.length; i++) {
+  //   let all_object = all_result[i];
+  //   let user_object = user_result[i];
+  //   let formatted_object = {
+  //     month: `${monthNames[all_object._id.month - 1]} ${user_object._id.year}`,
+  //     // myFood: user_object._id,
+  //     // myTransport: 90.354,
+  //     // myApparel: 14.472,
+  //     // mySocialLife: 28.597,
+  //     // myHousehold: 91.827,
+  //     // myGift: 20.362,
+  //     // myOther: 150.243,
+  //     // avgOther: 45.093,
+  //     // avgFood: 6.679,
+  //     // avgTransport: 28.638,
+  //     // avgApparel: 5.133,
+  //     // avgSocialLife: 6.333,
+  //     // avgHousehold: 27.693,
+  //     // avgGift: 8.318,
+  //   };
+  //   formatted_response.push(formatted_object);
+  // }
+
   //format response
 
-  console.log(user_result);
-  res.status(200).send(user_result);
+  let formatted_response = {
+    user: user_result.map((element) => {
+      return {
+        year: element._id.year,
+        month: element._id.month,
+        category: element._id.category,
+        average: Number(parseFloat(element.average.toString()).toFixed(2)),
+      };
+    }),
+    average: all_result.map((element) => {
+      return {
+        year: element._id.year,
+        month: element._id.month,
+        category: element._id.category,
+        average: Number(parseFloat(element.average.toString()).toFixed(2)),
+      };
+    }),
+  };
+  res.status(200).send(formatted_response);
   next();
 };
 
