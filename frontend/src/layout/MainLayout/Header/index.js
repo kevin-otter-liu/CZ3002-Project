62c9970b8f1183d4fd12197569a6c9fd9384c0cc
Stefanie Low <stefanie.low@hotmail.com>
2022-04-03 12:09:42 +0800

    added button for report generation

--- frontend/src/layout/MainLayout/Header/index.js
+++ frontend/src/layout/MainLayout/Header/index.js
@@ -6,10 +6,10 @@ import { Avatar, Box, ButtonBase } from '@mui/material';
 
 // project imports
 import LogoSection from '../LogoSection';
-import SearchSection from './SearchSection';
 import ProfileSection from './ProfileSection';
 import NotificationSection from './NotificationSection';
 import BankLoginSection from './BankLoginSection';
+import ReportGenerationSection from './ReportGenerationSection';
 
 // assets
 import { IconMenu2 } from '@tabler/icons';
@@ -56,16 +56,17 @@ const Header = ({ handleLeftDrawerToggle }) => {
                 </ButtonBase>
             </Box>
 
-            {/* header search */}
-            {/* <SearchSection /> */}
             <Box sx={{ flexGrow: 1 }} />
             <Box sx={{ flexGrow: 1 }} />
 
             {/* Bank Login */}
             <BankLoginSection />
 
+            {/* Report Generation button*/}
+            <ReportGenerationSection />
+
             {/* notification & profile */}
-            <NotificationSection />
+            {/* <NotificationSection /> */}
             <ProfileSection />
         </>
     );
